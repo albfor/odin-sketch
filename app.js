4cab1b271ff9e514b31eaa04af9862eb29cce0d7
Albin Forsberg <96afo02@gmail.com>
2022-07-01 13:02:45 +0200

    change box color on hover, gridsize prompt

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-
 const container = document.createElement("div");
 container.classList.add("container");
+const button = document.createElement("button");
+button.textContent = "Create New Grid";
+button.addEventListener('click', createGrid);
+document.body.appendChild(button);
 for (let i = 0; i < 16; i++) {
    const row = document.createElement("div");
    row.classList.add("row");
@@ -8,10 +11,21 @@ for (let i = 0; i < 16; i++) {
    for (let j = 0; j < 16; j++) {
       const box = document.createElement("div");
       box.classList.add("box");
-      box.textContent = "x";
+      box.addEventListener("mouseover", onHover);
       row.appendChild(box);
    }
    container.appendChild(row);
 }
 document.body.appendChild(container);
+
+function onHover() {
+   this.style.backgroundColor = "red";
+}
+
+
+function createGrid() {
+   let size = prompt('Grid Size:', '16');
+   console.log(size);
+}
+
 
